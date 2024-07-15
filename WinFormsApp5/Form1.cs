ec90c65fd5abb23c0b784a9be9a4e433323e7390
PhụngHuyTruong <truongphph48831@gmail.com>
2024-07-15 20:37:51 +0700

    Thông điệp commit của bạn

--- WinFormsApp5/Form1.cs
+++ WinFormsApp5/Form1.cs
@@ -1,10 +1,14 @@
-namespace WinFormsApp5
+﻿namespace WinFormsApp5
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        public void dddda()
+        {
+            MessageBox.Show("dấdad");
         }
     }
 }
